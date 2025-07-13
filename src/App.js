b5b1493a32a79f29626b20fa42c0124dcc8efc12
Vishal Tayade <tayadevishal00@gmail.com>
2025-07-13 15:14:19 +0530

    routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+import CalendarPage from "./pages/CalendarPage";
 
-function App() {
+export default function App() {
+
+  const auth = localStorage.getItem("login") === "true";
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/calendar" element={auth ? <CalendarPage /> : <Navigate to="/login" />} />
+      </Routes>
+    </Router>
   );
 }
-
-export default App;
